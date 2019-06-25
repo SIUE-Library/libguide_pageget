32ad219ed27006b3fa839d60e34c7823e294d482
Dale Auten <dauten96@gmail.com>
2019-06-25 13:15:11 -0500

    output looks cleaner

--- youtubewarn.py
+++ youtubewarn.py
@@ -7,11 +7,11 @@ regex = r"http:\/\/(www\.)?youtube.com"
 print(sys.argv[1])
 num = 0
 for line in open(sys.argv[1], "r"):
-	print("line %d" % num)
+	print("checking page %d..." % num)
 	num += 1
 	html = requests.get(line).text
 	matches = re.finditer(regex, html)
 	for match in matches:
-		print("on page %s http youtube link %s detected" % (line, line[match.start():match.end()]))
+		print("on page %s http youtube link %s... detected" % (line[:-1], html[match.start():match.end()+20]))
 
 print("done")
