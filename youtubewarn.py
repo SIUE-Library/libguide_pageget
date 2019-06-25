6432d4733821b78f547e5d49761fc3bd156b9709
Dale Auten <dauten96@gmail.com>
2019-06-25 14:48:01 -0500

    youtubewarn works

--- youtubewarn.py
+++ youtubewarn.py
@@ -2,16 +2,19 @@ import re
 import sys
 import requests
 
-regex = r"http:\/\/(www\.)?youtube.com"
+regex = r'"http:\/\/(www\.)?youtube.com\S*"'
 
 print(sys.argv[1])
+out = open("tomodify", "w")
+
 num = 0
 for line in open(sys.argv[1], "r"):
 	print("checking page %d..." % num)
 	num += 1
-	html = requests.get(line).text
+	html = requests.get("https://"+line).text
 	matches = re.finditer(regex, html)
 	for match in matches:
-		print("on page %s http youtube link %s... detected" % (line[:-1], html[match.start():match.end()+20]))
+		print("on page %s http youtube link %s... detected" % (line[:-1], html[match.start():match.end()]))
+#		write(out, line)
 
 print("done")
